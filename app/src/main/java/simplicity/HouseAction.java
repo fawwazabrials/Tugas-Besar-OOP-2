ca827f3ff7fe77c29bda70b12b0139b2f64268de
fawwazabrials <fawwazabrial.fas@gmail.com>
2023-04-19 13:48:44 +0700

    Add HouseAction interface

---
+++ app/src/main/java/simplicity/HouseAction.java
@@ -0,0 +1,13 @@
+package simplicity;
+
+public interface HouseAction {
+    public void addHouse(int x, int y);
+    /* 
+     * Tambah rumah dari World pada koordinat (x, y). Jika sudah ada rumah, method akan mengembalikan error
+     */
+
+    public void removeHouse(int x, int y);
+    /*
+     * Hapus rumah dari World pada koordinat (x, y). Jika tidak ada rumah, method akan mengembalikan error
+     */
+}
