3b1d6eb89f83e4e42bd25c768810cbc83af61953
Muhammad Naufal <naufalmuhammadmail@gmail.com>
2023-04-24 01:36:52 +0700

    membuat class House dan memperbaiki interface HouseAction

--- app/src/main/java/simplicity/HouseAction.java
+++ app/src/main/java/simplicity/HouseAction.java
@@ -1,13 +1,22 @@
 package simplicity;
 
 public interface HouseAction {
-    public void addHouse(int x, int y);
-    /* 
-     * Tambah rumah dari World pada koordinat (x, y). Jika sudah ada rumah, method akan mengembalikan error
+    public void addRoom(String roomName, Room benchmarkRoom, Direction direction);
+    /*
+     * Menambahkan Room baru pada House. Room baru akan bersebelahan dengan benchmarkRoom pada 
+     * arah direction. Jika benchmarkRoom tidak ada atau sudah bersebelahan dengan Room lain pada 
+     * arah direction, method akan mengembalikan error
      */
 
-    public void removeHouse(int x, int y);
+     public void removeRoom(Room room);
     /*
-     * Hapus rumah dari World pada koordinat (x, y). Jika tidak ada rumah, method akan mengembalikan error
+    * Menghapus Room dari House. Jika Room tidak ada, method akan mengembalikan error
+    */
+
+    public void moveRoom(Room room, Room benchmarkRoom, Direction direction);
+    /*
+     * Memindahkan Room dari House. Room akan bersebelahan dengan benchmarkRoom pada arah direction. 
+     * Jika benchmarkRoom tidak ada atau sudah bersebelahan dengan Room lain pada arah direction, 
+     * method akan mengembalikan error
      */
 }
