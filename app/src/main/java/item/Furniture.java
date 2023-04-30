a572a0463d8c847238022c3381a26e75da7646fb
sherraaa <102856322+sherraaa@users.noreply.github.com>
2023-04-30 21:02:34 +0700

    Update Furniture.java

--- app/src/main/java/item/Furniture.java
+++ app/src/main/java/item/Furniture.java
@@ -1,9 +1,99 @@
 package item;
 
 public class Furniture extends Item {
+    // Attributes
+    private String name;
+    private String category;
+    private int priceValue;
+    private int width;
+    private int length;
+
+    // Constructor
+    public Furniture(String name, int width, int length) {
+        this.name = name;
+        this.category = "furniture";
+        this.setPrice(name);
+        this.setLength(name);
+        this.setWidth(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getPriceValue() {
+        return priceValue;
+    }
+
+    public int getWidth() {
+        return width;
+    }
 
     public int getLength() {
-        return 0;
+        return length;
     }
-    
+
+    public void setPrice(String name) {
+        if (name.equals("kasur single")) {
+            this.priceValue = 50;
+        } else if (name.equals("kasur queen size")) {
+            this.priceValue = 100;
+        } else if (name.equals("kasur king size")) {
+            this.priceValue = 150;
+        } else if (name.equals("toilet")) {
+            this.priceValue = 50;
+        } else if (name.equals("kompor gas")) {
+            this.priceValue = 100;
+        } else if (name.equals("kompor listrik")) {
+            this.priceValue = 200;
+        } else if (name.equals("meja dan kursi")) {
+            this.priceValue = 50;
+        } else if (name.equals("jam")) {
+            this.priceValue = 10;
+        }
+    }
+
+    public void setLength(String name) {
+        if (name.equals("kasur single")) {
+            this.length = 4;
+        } else if (name.equals("kasur queen size")) {
+            this.length = 4;
+        } else if (name.equals("kasur king size")) {
+            this.length = 5;
+        } else if (name.equals("toilet")) {
+            this.length = 1;
+        } else if (name.equals("kompor gas")) {
+            this.length = 2;
+        } else if (name.equals("kompor listrik")) {
+            this.length = 2;
+        } else if (name.equals("meja dan kursi")) {
+            this.length = 3;
+        } else if (name.equals("jam")) {
+            this.length = 1;
+        }
+    }
+
+    public void setWidth(String name) {
+        if (name.equals("kasur single")) {
+            this.width = 1;
+        } else if (name.equals("kasur queen size")) {
+            this.width = 2;
+        } else if (name.equals("kasur king size")) {
+            this.width = 2;
+        } else if (name.equals("toilet")) {
+            this.width = 1;
+        } else if (name.equals("kompor gas")) {
+            this.width = 1;
+        } else if (name.equals("kompor listrik")) {
+            this.width = 1;
+        } else if (name.equals("meja dan kursi")) {
+            this.width = 3;
+        } else if (name.equals("jam")) {
+            this.width = 1;
+        }
+    }
 }
