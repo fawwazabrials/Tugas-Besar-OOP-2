010c11480b068299fd4872e30db2b01415a0aed7
fawwazabrials <fawwazabrial.fas@gmail.com>
2023-05-03 15:16:54 +0700

    Add method to convert seconds to timestamp

--- app/src/main/java/util/Angka.java
+++ app/src/main/java/util/Angka.java
@@ -8,4 +8,16 @@ public class Angka {
             return -999;
         }
     }
+
+    public static String secToTime(int secs) {
+        String time = "";
+        int minutes = secs / 60;
+        int seconds = secs % 60;
+
+        if (minutes >= 0) time += minutes + " menit";
+        if (minutes >= 0 && seconds >= 0) time += " ";
+        if (seconds >= 0) time += seconds + " detik";
+
+        return time;
+    }
 }
