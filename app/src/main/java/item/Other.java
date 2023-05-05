322ae7c5d6b45d9e04b6534cb989d11a573a3d95
fawwazabrials <fawwazabrial.fas@gmail.com>
2023-05-05 22:33:33 +0700

    Add items other than furniture and foods

---
+++ app/src/main/java/item/Other.java
@@ -0,0 +1,36 @@
+package item;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Other extends Item {
+    private static Map<String, String[]> availableOther;
+    
+    static {
+        // key : Nama barang
+        // value : [Harga barang, action]
+        availableOther = new HashMap<String, String[]>();
+        availableOther.put("HP", new String[] {"50", "Gamble"});
+        availableOther.put("Sheet QnA", new String[] {"0", "Read QnA"});
+    }
+
+    public static Map<String, String[]> getAvailableOther() {return availableOther;}
+
+    private String name;
+    private String category;
+    private String action;
+    private int priceValue;
+
+    public String getName() {return name;}
+    public String getCategory() {return category;}
+    public String getAction() {return action;}
+    public int getPriceValue() {return priceValue;}
+
+    public Other(String name) {
+        this.name = name;
+        this.category = "other";
+        this.priceValue = Integer.parseInt(availableOther.get(name)[0]);
+        this.action = availableOther.get(name)[1];
+    }
+    
+}
